351e50fe789e4839b82618d2eb208f95abe976c3
michelachinosi01 <michela.chinosi01@universitadipavia.it>
2016-04-28 13:13:40 +0200

    sviluppo collezionabili
    id autoincremento, classe di prova, e parte di codice per caricare lista figurine da file

--- Figurine/src/collezionabili/Figurina.java
+++ Figurine/src/collezionabili/Figurina.java
@@ -6,39 +6,51 @@ import utenti.*;
 
 
 public class Figurina {
+    /*
+        Autoincremento id figurina
+    */
+        private static int count;
+
+	private final String Nome;
+	private final int Id;
+        private int Rarità;
 	
-	private String Nome;
-	private int Id,Rarità;
-	
-	public Figurina(int i,String n,int r){
-		
-		
-		this.Id=i;
+        
+	public Figurina(String n,int r){
 		this.Nome=n;
 		this.Rarità=r;
+                this.Id=++count;
 	}
 	
-	//Metodi getter
-	
+
+        
 	public String getNome(){
-		
 		return this.Nome;
 	}
 	
-public int getId(){
-		
+        public int getId(){
 		return this.Id;
 	}
 
-public int getRarità(){
+        public int getRarità(){
+            return this.Rarità;
+        }
 	
-	return this.Rarità;
-}
-	
+        public boolean changeRar(int newP){
+            if (newP >0){
+                this.Rarità=newP;
+                return true;
+            }
+            else {
+                System.out.println("Valore di rarità non permessa");
+                return false;
+            }
+            
+        }
+        @Override
 	public String toString(){
-		
 		return (String)(this.getId()+" "+this.getNome()+" "+this.getRarità());
-	}
-	
-
+                
+        }
+        
 }
--- Figurine/src/collezionabili/Figurina_Interface.java
+++ Figurine/src/collezionabili/Figurina_Interface.java
@@ -10,17 +10,27 @@ import java.util.*;
 
 public interface Figurina_Interface {
 
-	public boolean addFigurina(String n,int i,int r);
-	public boolean removeFigurina(int i);
-	public int countFigurina();
-	public void sortAlfa();
-	public void sortRar();
-	public boolean addFigurina(Figurina f);
-	public Figurina getFigurinabyId(int i);
-	public ArrayList<Figurina> getFigurinabyNome(String n);
-	public boolean compareFigurine(Figurina f1,Figurina f2);
-	
-	
-	
-	
+    /*
+        Edit collezione
+    */
+    public boolean addFigurina(String n,UUID i,int r);
+    public boolean addFigurina(Figurina f);
+    public boolean removeFigurina(int i);
+        
+    public int countFigurina();
+        
+    /*
+        Ordinamento
+    */
+    public void sortAlfa();
+    public void sortRar();
+       
+    /*
+        Search
+    */
+    public Figurina getFigurinabyId(int i);
+    public ArrayList<Figurina> getFigurinabyNome(String n);
+        
+    public boolean compareFigurine(Figurina f1,Figurina f2);
+        	
 }
