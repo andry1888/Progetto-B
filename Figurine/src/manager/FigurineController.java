51f4f2b140368b0f5b7ce753ad0b9a38e4148db4
Biagio42 <Biagio.calicchio01@universitadipavia.it>
2016-06-02 18:49:01 +0200

    Add files via upload

--- Figurine/src/manager/FigurineController.java
+++ Figurine/src/manager/FigurineController.java
@@ -10,18 +10,42 @@ import utenti.*;
 public class FigurineController implements ActionListener {
 	
 	private Utente utente;  	   
-	private String[] nomef =new String[utente.getCollezione().size()];	
+	private String[] nomef; 
+	private int i;
 	
-	public FigurineController(Utente u){		
-			utente=u;		
+	public FigurineController(Utente u){
+		
+			utente=u;	
+			nomef=new String[utente.getCollezione().size()];
+			i=0;
 		}
+	
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {		
-		//aggiungo il nome della figurina all'array nomef		
-		for(int i=0;i<utente.getCollezione().size();i++){			
-			nomef[i]=e.getActionCommand();
-			}
+	public void actionPerformed(ActionEvent e) {
+		
+		while(i<utente.getCollezione().size()){
+			aggiungiFig(e.getActionCommand());
+			i++;
+		}
+		for(int j=0;j<i;j++) System.out.println(nomef[j]);
+		coloraFig(e.getSource());
+		
 		}
 	
+	
+	//aggiungo il nome della figurina all'array nomef
+	public void aggiungiFig(String nome){
+				
+			nomef[i]=nome;
+		}
+	
+	
+	//coloro nome figurina di verde
+	public void coloraFig(Object fig){
+		
+		JButton f=(JButton)fig;
+		f.setBackground(Color.GREEN);
+		
+	}
 }
