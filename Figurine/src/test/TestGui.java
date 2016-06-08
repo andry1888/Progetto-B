6079ae55a680a2174cc705aa1ef2d2899bc13581
Biagio42 <Biagio.calicchio01@universitadipavia.it>
2016-06-08 18:11:01 +0200

    Add files via upload

--- Figurine/src/test/TestGui.java
+++ Figurine/src/test/TestGui.java
@@ -20,6 +20,13 @@ public class TestGui {
 		Utente c1=new Collezionista("mario","rossi","via roma",30,100,port,"bimbo93","bamba","mail@yahoo.it");	
 		Utente c2=new Collezionista("luigi","verdi","via del corso",28,150,port,"thunderbolt","pecorino","hotmail@libero.com");	
 		
+		c1.addFigurina(new Figurina(1,"test",1000));
+		c1.addFigurina(new Figurina(2,"test2",1000));
+		c1.addFigurina(new Figurina(3,"test3",1000));
+		
+		c2.addFigurina(new Figurina(4,"test4",1000));
+		c2.addFigurina(new Figurina(5,"test5",1000));
+		c2.addFigurina(new Figurina(6,"test6",1000));
 		
 		Scambio s=new Scambio(c1,c2);
 		
